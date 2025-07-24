89605d6459592a1dcf8080f2a1089c9b12b3b5df
tomy.dev <xxx.d1231@gmail.com>
2025-07-24 14:15:15 +0900

    Client.jsのコメントを修正し、microCMSのAPIエンドポイントについて明確に記述

--- src/assets/libs/Client.js
+++ src/assets/libs/Client.js
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
-// microCMSを読み込むため記述,APIキーは.envに記述
+// microCMSのAPIエンドポイント
 export const client = createClient({
   serviceDomain: import.meta.env.VITE_SERVICE_DOMAIN,
   apiKey: import.meta.env.VITE_API_KEY,
