484d4a4ba9d0ff44042464f6bda21cce1cf31643
tomy.dev <xxx.d1231@gmail.com>
2025-07-25 10:28:44 +0900

    Client.jsからmicroCMSのAPIエンドポイントに関するコメントを削除

--- src/assets/libs/Client.js
+++ src/assets/libs/Client.js
@@ -1,6 +1,5 @@
 import { createClient } from "microcms-js-sdk";
 
-// microCMSのAPIエンドポイント
 export const client = createClient({
   serviceDomain: import.meta.env.VITE_SERVICE_DOMAIN,
   apiKey: import.meta.env.VITE_API_KEY,
